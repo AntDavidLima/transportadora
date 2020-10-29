29da93329da5d6b880148a37296503e5dcf1ca79
AntDavidLima <davidlima@alu.ufc.br>
2020-10-29 09:25:45 -0300

    Data visualization

--- frontend/src/pages/main/index.tsx
+++ frontend/src/pages/main/index.tsx
@@ -1,21 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import api from '../../services/api.js';
 
+import './styles.css';
+
+interface Trajeto {
+  id: string;
+  local: string;
+  created_at: string;
+  updated_at: string;
+  encomenda_id: string;
+}
+
 export default function Main() {
-  const [trajetos, setTrajetos] = useState([]);
+  const [trajetos, setTrajetos] = useState<Trajeto[]>([]);
+  const [produto, setProduto] = useState('');
 
-  useEffect(() => {
-    async function loadTrajetos() {
-      const { data } = await api.get(
-        'trajetos/6caa2d4d-d603-4268-8552-ec91368fddfa',
-      );
+  async function loadTrajetos() {
+    // eslint-disable-next-line no-restricted-globals
+    event?.preventDefault();
 
-      setTrajetos(data);
-    }
-    console.log(trajetos);
-    loadTrajetos();
-  }, []);
+    const { data } = await api.get(`trajetos/${produto}`);
 
-  return <h1>Teste</h1>;
+    setTrajetos(data);
+  }
+
+  return (
+    <div id="main">
+      <div id="busca">
+        <form onSubmit={loadTrajetos}>
+          <input
+            type="text"
+            placeholder="Código de rastreio:"
+            value={produto}
+            onChange={e => setProduto(e.target.value)}
+          />
+          <button type="submit">Rastrear</button>
+        </form>
+      </div>
+      <div id="trajetos">
+        {trajetos.map(trajeto => (
+          <div id="dados">
+            <p className="codigo" key={trajeto.id}>
+              {trajeto.encomenda_id}
+            </p>
+            <p>{trajeto.local}</p>
+            <p>{trajeto.updated_at}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
